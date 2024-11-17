8fa80a77157ef9b62cc819c931dd20e746fdf651
John Grassey <johngrassey@gmail.com>
2024-11-17 07:05:06 -0500

    Add functions to populate boards
    Hard coded coordinates for now. Will eventually
    replace with function that generates random
    coordinates.

--- controllers.js
+++ controllers.js
@@ -13,6 +13,21 @@ class gameController {
     this.activePlayer =
       this.activePlayer === this.player1 ? this.player2 : this.player1;
   }
-}
 
+  *populateBoard(player1) {
+    player1.board.placeShip(0, 0, player1.board.ships.carrier, "horizontal");
+    player1.board.placeShip(1, 0, player1.board.ships.battleship, "horizontal");
+    player1.board.placeShip(2, 0, player1.board.ships.cruiser, "horizontal");
+    player1.board.placeShip(3, 0, player1.board.ships.submarine, "horizontal");
+    player1.board.placeShip(4, 0, player1.board.ships.destroyer, "horizontal");
+  }
+
+  *populateBoard(player2) {
+    player2.board.placeShip(0, 0, player2.board.ships.carrier, "vertical");
+    player2.board.placeShip(0, 1, player2.board.ships.battleship, "vertical");
+    player2.board.placeShip(0, 2, player2.board.ships.cruiser, "vertical");
+    player2.board.placeShip(0, 3, player2.board.ships.submarine, "vertical");
+    player2.board.placeShip(0, 4, player2.board.ships.destroyer, "vertical");
+  }
+}
 export { gameController };
