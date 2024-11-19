8b476eca6ea5ad8415c9190d6c1581a80feb3e58
John Grassey <johngrassey@gmail.com>
2024-11-18 21:52:32 -0500

    Render Player Board
    Laying out the board that shows the player's own
    ships and the opponent's hits and misses.

--- src/screenController.js
+++ src/screenController.js
@@ -8,25 +8,21 @@ class screenController {
   updatePlayerBoard() {
     const board = this.gameController.activePlayer.board.getBoard();
     const boardDiv = document.querySelector("#playerboard");
-    boardDiv.innerHTML = "";
-    board.forEach((row, i) => {
-      console.log("bananas");
-      row.forEach((cell, j) => {
+    board.forEach((row) => {
+      const rowDiv = document.createElement("div");
+      rowDiv.classList.add("row");
+      row.forEach((cell) => {
         const cellDiv = document.createElement("div");
-        cellDiv.textContent = cell;
-        cellDiv.dataset.x = i;
-        cellDiv.dataset.y = j;
-        // cellDiv.addEventListener("click", (e) => {
-        //   this.gameController.activePlayer.attack(
-        //     parseInt(e.target.dataset.x),
-        //     parseInt(e.target.dataset.y),
-        //     this.gameController
-        //   );
-        //   this.updateActiveBoard();
-        //   this.updateInactiveBoard();
-        // });
-        boardDiv.appendChild(cellDiv);
+        cellDiv.classList.add("cell");
+        if (cell.length > 1) {
+          cellDiv.classList.add("ship");
+          cellDiv.textContent = cell;
+        } else {
+          cellDiv.textContent = cell;
+        }
+        rowDiv.appendChild(cellDiv);
       });
+      boardDiv.appendChild(rowDiv);
     });
   }
 
