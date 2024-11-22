549f0900577a4ac3eb5d8e18fa68778569474290
John Grassey <johngrassey@gmail.com>
2024-11-22 14:44:48 -0500

    Add Opponent Board
    Game displays the opponent's board. Need to still
    hide irrelevant information from the active player

--- src/gameController.js
+++ src/gameController.js
@@ -7,14 +7,17 @@ class gameController {
     this.player1 = new Player("Player 1", "human");
     this.player2 = new Player("Player 2", "computer");
     this.activePlayer = this.player1;
+    this.inactivePlayer = this.player2;
   }
 
   switchPlayer() {
-    this.activePlayer =
-      this.activePlayer === this.player1 ? this.player2 : this.player1;
+    [this.activePlayer, this.inactivePlayer] = [
+      this.inactivePlayer,
+      this.activePlayer,
+    ];
   }
 
-  populateBoard(player1) {
+  populatePlayerOneBoard(player1) {
     player1.board.placeShip(0, 0, player1.board.ships.carrier, "horizontal");
     player1.board.placeShip(1, 0, player1.board.ships.battleship, "horizontal");
     player1.board.placeShip(2, 0, player1.board.ships.cruiser, "horizontal");
@@ -22,12 +25,12 @@ class gameController {
     player1.board.placeShip(4, 0, player1.board.ships.destroyer, "horizontal");
   }
 
-  populateBoard(player2) {
-    player2.board.placeShip(0, 0, player2.board.ships.carrier, "vertical");
-    player2.board.placeShip(0, 1, player2.board.ships.battleship, "vertical");
-    player2.board.placeShip(0, 2, player2.board.ships.cruiser, "vertical");
-    player2.board.placeShip(0, 3, player2.board.ships.submarine, "vertical");
-    player2.board.placeShip(0, 4, player2.board.ships.destroyer, "vertical");
+  populatePlayerTwoBoard(player1) {
+    player1.board.placeShip(0, 0, player1.board.ships.carrier, "vertical");
+    player1.board.placeShip(0, 1, player1.board.ships.battleship, "vertical");
+    player1.board.placeShip(0, 2, player1.board.ships.cruiser, "vertical");
+    player1.board.placeShip(0, 3, player1.board.ships.submarine, "vertical");
+    player1.board.placeShip(0, 4, player1.board.ships.destroyer, "vertical");
   }
 }
 
--- src/screenController.js
+++ src/screenController.js
@@ -26,10 +26,32 @@ class screenController {
     });
   }
 
+  updateOppBoard() {
+    const board = this.gameController.inactivePlayer.board.getBoard();
+    const boardDiv = document.querySelector("#oppboard");
+    board.forEach((row) => {
+      const rowDiv = document.createElement("div");
+      rowDiv.classList.add("row");
+      row.forEach((cell) => {
+        const cellDiv = document.createElement("div");
+        cellDiv.classList.add("cell");
+        if (cell.length > 1) {
+          cellDiv.classList.add("ship");
+          cellDiv.textContent = cell;
+        } else {
+          cellDiv.textContent = cell;
+        }
+        rowDiv.appendChild(cellDiv);
+      });
+      boardDiv.appendChild(rowDiv);
+    });
+  }
+
   startGame() {
-    this.gameController.populateBoard(this.gameController.player1);
-    this.gameController.populateBoard(this.gameController.player2);
+    this.gameController.populatePlayerOneBoard(this.gameController.player1);
+    this.gameController.populatePlayerTwoBoard(this.gameController.player2);
     this.updatePlayerBoard();
+    this.updateOppBoard();
   }
 }
 
